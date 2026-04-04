d88d03f6ec754142ccf00b969a729a118764628b
Reuben <reubenelliott12345@gmail.com>
2026-04-04 03:19:33 +0100

    Add music indicator to status bar

---
+++ zellij/.local/bin/song.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+MAX_LEN=30
+
+song_name=$(playerctl metadata --format "{{ title }}" --player=$1)
+status=$(playerctl status --player=$1)
+
+# Truncate
+if [ ${#song_name} -gt $MAX_LEN ]; then
+    display_text="${song_name:0:$MAX_LEN}..."
+elif [ ${#song_name} -gt 0 ]; then
+    display_text="$song_name"
+else
+  display_text="󰽳" 
+fi
+
+# Icon
+if [[ "$status" == "Playing" ]]; then
+  icon="󰏤 "
+elif [[ "$status" == "Paused" ]]; then
+  icon=" "
+else
+  icon=""
+fi
+
+echo "$display_text $icon"
