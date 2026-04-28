26b7c3048386197b7c312436bd731f4c7e6c36d0
Reuben <reubenelliott12345@gmail.com>
2026-04-28 11:37:54 +0100

    Trouble config

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,10 +1,17 @@
 return {
 	"folke/trouble.nvim",
-	opts = {},
+	opts = {
+		focus = true,
+		auto_preview = false,
+	},
 	cmd = "Trouble",
 	keys = {
 		{ "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>", desc = "[T]oggle Diagnostics (Trouble)" },
-		{ "<leader>tT", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "[T]oggle Buffer Diagnostics (Trouble)" },
-		{ "<leader>ts", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "[T]oggle [S]ymbols (Trouble)" },
+		{
+			"<leader>tT",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "[T]oggle Buffer Diagnostics (Trouble)",
+		},
+		{ "<leader>ts", "<cmd>Trouble symbols toggle focus=true<cr>", desc = "[T]oggle [S]ymbols (Trouble)" },
 	},
 }
